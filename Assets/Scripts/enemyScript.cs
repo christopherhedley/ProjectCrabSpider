06d3d4cb8cf9247cd4e3cd200bf1007d2ea13c57
Jean-Paul <imurhero2@users.noreply.github.com>
2019-01-30 17:40:28 -0500

    Initial Commit (real)

---
+++ Assets/Scripts/enemyScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyScript : MonoBehaviour
+{
+    public float aggroRange = 500f; 
+    public Transform target;
+    private bool inRange;
+
+    Vector3 destination;
+    Vector3 startPosition;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        StartCoroutine("DistanceCheckDelay");
+
+        if (target.position.y < 1.5 && inRange)
+        {
+            agent.speed = 10;
+            agent.destination = target.position;
+        }
+        else
+        {
+            agent.speed = 3;
+            agent.destination = startPosition;
+        }
+    }
+
+    void DistanceCheck()
+    {
+        if (Vector3.Distance(target.position, transform.position) < aggroRange)
+        {
+            inRange = true;
+        }
+        else
+        {
+            inRange = false;
+        }
+    }
+
+    // Optimizes performance by checking for distance from player every .1 seconds
+    IEnumerator DistanceCheckDelay()
+    {
+        DistanceCheck();
+        yield return new WaitForSeconds(.1f);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, aggroRange);
+    }
+
+    /*
+     * void Scatter()
+    {
+        agent.destination.x = transform.position.x + Random.Range(-10.0f, 10.0f);
+        agent.destination.z = transform.position.z + Random.Range(-10.0f, 10.0f);
+    }
+    */
+}
---
+++ Assets/Scripts/playerScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerScript : MonoBehaviour {
+
+    public Text spiderAttachedText;
+    public Image screenBlock1;
+    public Image screenBlock2;
+    public Image screenBlock3;
+    private int spidersAttached = 0;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spiderAttachedText.text = "Spiders Attached: " + spidersAttached;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            spidersAttached += 1;
+            other.gameObject.SetActive(false);
+
+            if (spidersAttached == 1)
+            {
+                screenBlock1.gameObject.SetActive(true);
+            }
+            if (spidersAttached == 2)
+            {
+                screenBlock2.gameObject.SetActive(true);
+            }
+            if (spidersAttached == 3)
+            {
+                screenBlock3.gameObject.SetActive(true);
+            }
+
+            Debug.Log(other.name);
+        }
+    }
+}
