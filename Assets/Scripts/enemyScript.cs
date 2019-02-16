b47402c34b2226d0b466ea95d321b2d7d7058347
Jean-Paul <imurhero2@users.noreply.github.com>
2019-02-16 15:20:07 -0500

    Patrolling Enemies Update
    - Added ability for enemies to patrol in 10x10 area

--- Assets/Scripts/enemyScript.cs
+++ Assets/Scripts/enemyScript.cs
@@ -8,6 +8,7 @@ public class enemyScript : MonoBehaviour
     public float aggroRange = 500f; 
     public Transform target;
     private bool inRange;
+    private Vector3[] points;
 
     Vector3 destination;
     Vector3 startPosition;
@@ -17,12 +18,23 @@ public class enemyScript : MonoBehaviour
     {
         agent = GetComponent<NavMeshAgent>();
         startPosition = transform.position;
+        points = new[]
+{
+            new Vector3(startPosition.x + 5f, startPosition.y, startPosition.z + 5f),
+            new Vector3(startPosition.x + -5f, startPosition.y, startPosition.z + -5f),
+            new Vector3(startPosition.x + -5f, startPosition.y, startPosition.z + 5f),
+            new Vector3(startPosition.x, startPosition.y, startPosition.z + 5f),
+            new Vector3(startPosition.x, startPosition.y, startPosition.z + -5f),
+            new Vector3(startPosition.x + 5f, startPosition.y, startPosition.z),
+            new Vector3(startPosition.x + -5f, startPosition.y, startPosition.z),
+            startPosition
+        };
+        StartCoroutine("DistanceCheckDelay");
+        StartCoroutine("EnemyPatrol");
     }
 
     void Update()
     {
-        StartCoroutine("DistanceCheckDelay");
-
         if (!playerScript.inSafeZone && inRange)
         {
             agent.speed = 10;
@@ -31,7 +43,6 @@ public class enemyScript : MonoBehaviour
         else
         {
             agent.speed = 3;
-            agent.destination = startPosition;
         }
     }
 
@@ -47,24 +58,29 @@ public class enemyScript : MonoBehaviour
         }
     }
 
-    // Optimizes performance by checking for distance from player every .1 seconds
+    // Optimizes performance by checking for distance from player every .1 seconds instead of every frame
     IEnumerator DistanceCheckDelay()
     {
-        DistanceCheck();
-        yield return new WaitForSeconds(.1f);
+        for (; ; )
+        {
+            DistanceCheck();
+            yield return new WaitForSeconds(0.1f);
+        } 
+    }
+
+    IEnumerator EnemyPatrol()
+    {
+        for (; ; )
+        {
+            agent.destination = points[Random.Range(0, points.Length - 1)];
+            yield return new WaitForSeconds(Random.Range(3.0f, 5.0f));
+        }
     }
 
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(transform.position, aggroRange);
+        Gizmos.DrawWireSphere(transform.position, aggroRange); // Shows Aggro Range
+        Gizmos.DrawWireCube(transform.position, new Vector3(10, 1, 10)); // Show Patrol Range
     }
-
-    /*
-     * void Scatter()
-    {
-        agent.destination.x = transform.position.x + Random.Range(-10.0f, 10.0f);
-        agent.destination.z = transform.position.z + Random.Range(-10.0f, 10.0f);
-    }
-    */
 }
