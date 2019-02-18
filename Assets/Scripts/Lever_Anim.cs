82780433c359388e502126a712a6b204ad9ad60e
christopherhedley <christopher.g.hedley@knights.ucf.edu>
2019-02-18 15:40:22 -0500

    Added Lever textures and animation

---
+++ Assets/Scripts/Lever_Anim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever_Anim : MonoBehaviour {
+
+    public Animator anim;
+    public SphereCollider sctouch;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        sctouch = GetComponent<SphereCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.SetBool("PlayerNear", true);
+    }
+}
